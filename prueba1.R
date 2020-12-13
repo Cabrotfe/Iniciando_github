0ad07258b06c9742baea52bb44b4a3fcc89bc65a
Cabrotfe <cabrotfe@uc.cl>
2020-12-13 18:59:40 -0300

    Agregué un scrip con un ejemplo simple de tidy

---
+++ prueba1.R
@@ -0,0 +1,10 @@
+
+pacman::p_load(tidyverse,stringr, tidytext)
+
+mtcars=mtcars
+
+## Leseras para guardar
+
+?unnest_tokens
+tokens=mtcars %>% mutate(nombre = rownames(mtcars)) %>% tidytext::unnest_tokens(input=nombre,output="modelos",token = "words", drop = F)
+
